a4719aad8a820a5fc7acc840d21f547dc8296410
Adaka.Lavanya <alavanya@sparkgenieit.com>
2024-02-09 11:33:16 +0530

    JP-70 : UserProfileValidation

--- src/pages/common/UserProfile.js
+++ src/pages/common/UserProfile.js
@@ -7,6 +7,223 @@ import { useState } from 'react';
 
 function UserProfile() {
 
+  const [firstname, setFirstName] = useState("");
+  const [lastname, setLastname] = useState("");
+  const [email, setEmail] = useState("");
+  const [personal, setPersonal] = useState("");
+  const [show, setShow] = useState("");
+  const [visa, setVisa] = useState("");
+  const [cv, setCv] = useState("");
+  const [coverLetter, setCoverLetter] = useState("");
+
+
+
+  const [firstnameError, setFirstnameError] = useState("Please Enter First Name");
+  const [lastnameError, setLastnameError] = useState("please Enter Last Name");
+  const [emailError, setEmailError] = useState("Please Enter Email");
+  const [personalsummaryError, setPersonalsummaryError] = useState("Enter Personal Summary");
+  const [showprofileError, setShowprofileError] = useState("Please Select Show Profile")
+  const [visaError, setVisaError] = useState("Please Select Visa")
+  const [cvError, setCvError] = useState("Please Upload CV")
+  const [coverLetterError, setCoverLetterError] = useState("Please Upload Cover Letter")
+
+
+
+
+
+  const [errors, setErrors] = useState({
+    firstname: false,
+    lastname: false,
+    email: false,
+    personal: false,
+    show: false,
+    visa: false,
+    cv: false,
+    coverLetter: false,
+
+
+  })
+
+
+  const chnageOut = (name, event) => {
+    if (name == "firstname") {
+      if (event.target.value == "") {
+        setErrors({ ...errors, firstname: true })
+      }
+      else {
+        setErrors({ ...errors, firstname: false })
+
+      }
+      setFirstName(event.target.value);
+
+    }
+
+    if (name == "lastname") {
+      if (event.target.value == "") {
+        setErrors({ ...errors, lastname: true })
+      }
+      else {
+        setErrors({ ...errors, lastname: false })
+      }
+      setLastname(event.target.value)
+
+    }
+    if (name == 'email') {
+      if (event.target.value == "") {
+        setErrors({ ...errors, email: true })
+      }
+      else {
+        setErrors({ ...errors, email: false })
+      }
+      setEmail(event.target.value)
+
+    }
+    if (name == 'personal') {
+      if (event.target.value == "") {
+        setErrors({ ...errors, personal: true })
+      }
+      else {
+        setErrors({ ...errors, personal: false })
+      }
+      setPersonal(event.target.value)
+
+    }
+    if (name == 'show') {
+      if (event.target.value == "") {
+        setErrors({ ...errors, show: true })
+      }
+      else {
+        setErrors({ ...errors, show: false })
+      }
+      setShow(event.target.value)
+
+    }
+    if (name == "visa") {
+      if (event.target.value == "") {
+        setErrors({ ...errors, visa: true })
+      }
+
+      else {
+        setErrors({ ...errors, visa: false })
+      }
+      setVisa(event.target.value)
+
+    }
+    if (name == "cv") {
+      if (event.target.value == "") {
+        setErrors({ ...errors, cv: true })
+      }
+
+      else {
+        setErrors({ ...errors, cv: false })
+      }
+      setCv(event.target.value)
+
+    }
+    if (name == "coverLetter") {
+      if (event.target.value == "") {
+        setErrors({ ...errors, coverLetter: true })
+      }
+
+      else {
+        setErrors({ ...errors, coverLetter: false })
+      }
+      setCoverLetter(event.target.value)
+
+    }
+  }
+
+
+
+  const SubmitData = () => {
+    let obj = {}
+
+    if (firstname == "") {
+      obj = { ...obj, firstname: true }
+      setFirstnameError("Please Enter First Name")
+    }
+    else if (/^[a-z]{3,}$/gi.test(firstname) == false) {
+      obj = { ...obj, firstname: true }
+      setFirstnameError("Invalid First Name")
+    }
+    else {
+      obj = { ...obj, firstname: false }
+    }
+
+    if (lastname == "") {
+      obj = { ...obj, lastname: true }
+      setLastnameError("Please Enter Last Name")
+    }
+    else if (/^[a-z]{3,}$/gi.test(lastname) == false) {
+      obj = { ...obj, lastname: true }
+      setLastnameError("Invalid Last Name")
+    }
+    else {
+      obj = { ...obj, lastname: false }
+    }
+    if (email == "") {
+      obj = { ...obj, email: true }
+      setEmailError("Please Enter Email")
+    }
+    else if (/^[a-z A-Z 0-9._-]+@[a-z A-Z 0-9.-]+\.[a-z A-Z]{2,4}$/.test(email) == false) {
+      obj = { ...obj, email: true }
+      setEmailError("Invalid Email")
+    }
+    else {
+      obj = { ...obj, email: false }
+    }
+    if (personal == "") {
+      obj = { ...obj, personal: true }
+      setPersonalsummaryError("Please Enter Personal Data")
+
+    }
+    else if (/^[a-z]{3,}$/gi.test(personal) == false) {
+      obj = { ...obj, personal: true }
+      setPersonalsummaryError("Invalid Personal Data")
+    }
+    else {
+      obj = { ...obj, personal: false }
+    }
+    if (show == "") {
+      obj = { ...obj, show: true }
+      setShowprofileError("Please Select Profile");
+
+    }
+    else if (/^[a-z]{3,}$/gi.test(show) == false) {
+      obj = { ...obj, show: true }
+      setShowprofileError("Invalid Profile");
+    }
+    else {
+      obj = { ...obj, show: false }
+    }
+    if (visa == "") {
+      obj = { ...obj, visa: true }
+    }
+    else {
+      obj = { ...obj, visa: false }
+    }
+
+    if (cv == "") {
+      obj = { ...obj, cv: true }
+    }
+    else {
+      obj = { ...obj, cv: false }
+    }
+
+    if (coverLetter == "") {
+      obj = { ...obj, coverLetter: true }
+    }
+    else {
+      obj = { ...obj, coverLetter: false }
+    }
+
+
+    setErrors(obj)
+
+
+  }
+
+
   const [work, setWork] = useState(0);
   const [education, setEducation] = useState(0);
   const [licence, setLicence] = useState(0);
@@ -64,7 +281,10 @@ function UserProfile() {
                       <div className="form-group row">
                         <label className="col-sm-3 col-form-label">First Name <span style={{ color: "red" }}>*</span></label>
                         <div className="col-sm-9">
-                          <input type="text" className="form-control" />
+                          <input type="text" className="form-control" value={firstname} onChange={(event) => chnageOut("firstname", event)} />
+
+                          {errors && errors.firstname && <div className="error text-danger"> {firstnameError}</div>}
+
                         </div>
                       </div>
                     </div>
@@ -74,7 +294,9 @@ function UserProfile() {
                       <div className="form-group row">
                         <label className="col-sm-3 col-form-label">Last Name <span style={{ color: "red" }}>*</span></label>
                         <div className="col-sm-9">
-                          <input type="text" className="form-control" />
+                          <input type="text" className="form-control" value={lastname} onChange={(event) => chnageOut("lastname", event)} />
+
+                          {errors && errors.lastname && <div className="error text-danger"> {lastnameError}</div>}
                         </div>
                       </div>
                     </div>
@@ -82,9 +304,11 @@ function UserProfile() {
                   <div className="row">
                     <div className="col-md-9">
                       <div className="form-group row">
-                        <label className="col-sm-3 col-form-label">Email address <span style={{ color: "red" }}>*</span></label>
+                        <label className="col-sm-3 col-form-label">Email <span style={{ color: "red" }}>*</span></label>
                         <div className="col-sm-9">
-                          <input type="email" className="form-control" />
+                          <input type="text" className="form-control" value={email} onChange={(event) => chnageOut("email", event)} />
+
+                          {errors && errors.email && <div className="error text-danger"> {emailError}</div>}
                         </div>
                       </div>
                     </div>
@@ -102,9 +326,11 @@ function UserProfile() {
                   <div className="row">
                     <div className="col-md-9">
                       <div className="form-group row">
-                        <label className="col-sm-6 col-form-label">Personal Summary<span style={{ color: "red" }}>*</span></label>
-                        <div className="col-sm-12">
-                          <textarea type="text" className="form-control" ></textarea>
+                        <label className="col-sm-3 col-form-label">Personal Summary<span style={{ color: "red" }}>*</span></label>
+                        <div className="col-sm-9">
+                          <textarea type="text" className="form-control" value={personal} onChange={(event) => chnageOut("personal", event)}></textarea>
+
+                          {errors && errors.personal && <div className="error text-danger"> {personalsummaryError}</div>}
 
                         </div>
                       </div>
@@ -209,7 +435,7 @@ function UserProfile() {
 
                         <div className="col-md-2">
                           <label className="col-sm-3 col-form-label my-1"><small>year Completed</small></label>
-                          <input type="text" className="form-control" />
+                          <input type="number" className="form-control" />
                         </div>
 
                         <div className="col-md-2">
@@ -238,7 +464,7 @@ function UserProfile() {
 
                                 <div className="col-md-2">
                                   <label className="col-sm-3 col-form-label my-1"><small>year Completed</small></label>
-                                  <input type="text" className="form-control" />
+                                  <input type="number" className="form-control" />
                                 </div>
 
                                 <div className="col-md-2">
@@ -289,7 +515,7 @@ function UserProfile() {
 
                         <div className="col-md-2">
                           <label className="col-sm-3 col-form-label"><small>Issue Date</small></label>
-                          <input type="text" className="form-control" />
+                          <input type="date" className="form-control" />
                         </div>
 
                         <div className="col-md-2">
@@ -383,13 +609,13 @@ function UserProfile() {
                         <div className="col-md-2">
                           <label className="col-sm-3 col-form-label"><small>Issue Date</small></label>
 
-                          <input type="text" className="form-control" />
+                          <input type="date" className="form-control" />
                         </div>
 
                         <div className="col-md-2">
                           <label className="col-sm-3 col-form-label"><small>Expiry Date</small></label>
 
-                          <input type="text" className="form-control" />
+                          <input type="date" className="form-control" />
                         </div>
                         <div className="col-md-2">
                           <label className="col-sm-3 col-form-label"><small>valid inNZ?</small></label>
@@ -574,13 +800,15 @@ function UserProfile() {
                   <div className="row">
                     <div className="col-md-9">
                       <div className="form-group row">
+
                         <label className="col-sm-3 col-form-label">Show Profile <span style={{ color: "red" }}>*</span></label>
                         <div className="col-sm-9">
-                          <select type="dropdown" className="form-control form-control form-control-lg">
+                          <select type="dropdown" className="form-control" value={show} onChange={(event) => chnageOut("show", event)}>
                             <option>---Select---</option>
                             <option>yes</option>
                             <option>No</option>
                           </select>
+                          {errors && errors.show && <div className="error text-danger"> {showprofileError}</div>}
 
                         </div>
                       </div>
@@ -592,37 +820,57 @@ function UserProfile() {
                   <div className="row">
                     <div className="col-md-6">
                       <div class="form-group row">
-                        <label className="col-form-label col-6">Visa Type</label>
-                        <select id="visa" onkeyup="ha29(event)" className="form-control col-6">
-                          <option>----Select----</option>
-                          <option>Working holiday visa</option>
-                          <option>Work visa</option>
-                          <option>Student visa</option>
-                          <option>Permanet Resident</option>
-                          <option>NZ citizen</option>
-                          <option>Others </option>
 
-                        </select>
+
+                        <label className="col-sm-3 col-form-label">Visa Type <span style={{ color: "red" }}>*</span></label>
+                        <div className="col-sm-12">
+                          <select type="dropdown" className="form-control" value={visa} onChange={(event) => chnageOut("visa", event)}>
+                            <option>---Select---</option>
+                            <option>----Select----</option>
+                            <option>Working holiday visa</option>
+                            <option>Work visa</option>
+                            <option>Student visa</option>
+                            <option>Permanet Resident</option>
+                            <option>NZ citizen</option>
+                            <option>Others </option>
+                          </select>
+                          {errors && errors.visa && <div className="error text-danger"> {visaError}</div>}
+                        </div>
                       </div>
                     </div>
 
-                    <div className="col-md-6">
+                    {/* <div className="col-md-6">
                       <div className="form-group row">
-                        <label className="col-form-label">Visa Expiry Date <span style={{ color: "red" }}>*</span></label>
-                        <div className="col-sm-12">
+                        <label className="col-form-label">Visa Expiry Date</label>
+                        <div className="col-sm-9">
                           <input className="form-control" type="date" />
                           <div className="bgred" id="error36"></div>
                         </div>
                       </div>
+                    </div> */}
+                     {/* <div className="row"> */}
+                    <div className="col-md-6">
+                      <div className="form-group row">
+                        <label className="col-sm-6 col-form-label">Visa Expiry Date </label>
+                        <div className="col-sm-12">
+                        <input className="form-control" type="date" />
+                        </div>
+                      </div>
                     </div>
                   </div>
+                  
 
                   <div className="row">
                     <div className="col-md-9">
                       <div className="form-group row">
-                        <label className="col-sm-3 col-form-label">Upload CV <span style={{ color: "red" }}>*</span></label>
+
+                        <label className="col-sm-3 col-form-label">Upload CV  <span style={{ color: "red" }}>*</span></label>
                         <div className="col-sm-9">
-                          <input type="file" className="form-control" />
+                          <input type="file" className="form-control" value={cv} onChange={(event) => chnageOut("cv", event)} />
+
+                          {errors && errors.cv && <div className="error text-danger"> {cvError}</div>}
+
+
                         </div>
                       </div>
                     </div>
@@ -631,30 +879,26 @@ function UserProfile() {
                   <div className="row">
                     <div className="col-md-9">
                       <div className="form-group row">
+                        {/* <label className="col-sm-3 col-form-label">Upload Cover Letter <span style={{ color: "red" }}>*</span></label>
+                        <div className="col-sm-9">
+                          <input type="file" className="form-control" /> */}
                         <label className="col-sm-3 col-form-label">Upload Cover Letter <span style={{ color: "red" }}>*</span></label>
                         <div className="col-sm-9">
-                          <input type="file" className="form-control" />
+                          <input type="file" className="form-control" value={coverLetter} onChange={(event) => chnageOut("coverLetter", event)} />
+
+                          {errors && errors.coverLetter && <div className="error text-danger"> {coverLetterError}</div>}
+
                         </div>
                       </div>
                     </div>
                   </div>
-
-
-                  <div className="row">
-                    <div className="col-md-6 form-check form-check-flat form-check-primary">
-                      <label className="form-check-label">
-                        <input type="checkbox" className="form-check-input" /> Remember me </label>
-                    </div>
-                  </div>
-
-
 
                   <div className="col-md-9 mt-3 row">
                     <div className="col-md-6">
                     </div>
                     <div className="col-md-6">
 
-                      <button type="button" className="btn btn-gradient-primary me-2">Submit</button>
+                      <button type="button" onClick={() => SubmitData()} className="btn btn-gradient-primary me-2">SAVE</button>
                     </div>
                   </div>
                 </form>
@@ -680,5 +924,6 @@ function UserProfile() {
     <Footer />
   </>
 }
+
 
 export default UserProfile;
