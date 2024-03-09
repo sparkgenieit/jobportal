7ed0e751e040bc372529072e982ec25e3e344d92
faraz9901 <mdfaraz9901@gmail.com>
2024-03-09 16:37:09 +0530

    Added the files with correct names

---
+++ src/pages/superadmin/user/UserList.js
@@ -0,0 +1,19 @@
+import Header from "../../../layouts/superadmin/Header";
+import Sidebar from "../../../layouts/superadmin/Sidebar";
+import Footer from "../../../layouts/superadmin/Footer";
+function UserList() {
+    return (
+
+        <>
+
+            <div>
+                <Header />
+                <div>
+                    <Sidebar />
+                </div>
+                <Footer />
+            </div>
+        </>
+    )
+}
+export default UserList;
