d998825317cee49e082a6f03e0dc5528aa01fcce
faraz9901 <mdfaraz9901@gmail.com>
2024-04-24 20:43:52 +0530

    Plans and Ads Creation

--- src/pages/common/ads.js
+++ src/pages/common/ads.js
@@ -1,12 +1,51 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
+import axios from 'axios';
 import './ads.css';
+import { useEffect, useState } from 'react';
 
-function ads() {
+function Ads() {
+  const [showAdvertisement, setShowAdvertisement] = useState(false);
+  const [ad, setAd] = useState(null)
+
+  useEffect(() => {
+    axios.get("http://localhost:8080/ads/all")
+      .then((response) => {
+        let allAds = response.data
+        const randomAd = Math.floor(Math.random() * allAds.length)
+        setAd(allAds[randomAd])
+      }
+      )
+      .catch(err => console.log(err))
+
+    setTimeout(() => {
+      setShowAdvertisement(true)
+    }, 2000)
+  }, [])
+
+
+  const handleClose = () => {
+    setShowAdvertisement(false);
+  };
+
   return (
-   <>
+    <>
+      {showAdvertisement && (
+        <div className="advertisement ">
+          <button className="close-btn" onClick={handleClose}>
+            &times;
+          </button>
+          <img src="https://via.placeholder.com/150" alt="Advertisement" />
+          <div className="advertisement-content">
+            <small>Ads</small>
+            <h2>{ad && ad.title}</h2>
+            <p>{ad && ad.description}</p>
+            <button>Shop Now</button>
+          </div>
+        </div>
+      )}
 
-   </>
+    </>
   );
 }
 
-export default App;
+export default Ads;
