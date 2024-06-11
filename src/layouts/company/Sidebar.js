b30f84c9cc17be17f79e7e8cb49e5533d90093cc
faraz9901 <mdfaraz9901@gmail.com>
2024-06-11 16:11:45 +0530

    Modal in Sidebar

--- src/layouts/company/Sidebar.js
+++ src/layouts/company/Sidebar.js
@@ -1,10 +1,15 @@
-import { useContext } from 'react';
+import { useContext, useState } from 'react';
 import './Sidebar.css';
 import { SidebarContext } from '../../helpers/Context';
+import { Modal } from "react-bootstrap";
 
 function Sidebar() {
   const { showSidebar } = useContext(SidebarContext)
   let sidebarClass = showSidebar ? "sidebar-showing" : "sidebar-not-showing";
+  const [show, setShow] = useState(false)
+  const handleClose = () => {
+    setShow(false)
+  }
   return (
     <>
       {<div className={`${sidebarClass}`}>
@@ -48,7 +53,7 @@ function Sidebar() {
 
               <ul class="nav sub-nav">
                 <li class="nav-item">
-                  <a class="nav-link" href="/company/postajob">
+                  <a class="nav-link" onClick={() => { setShow(true) }}>
                     <span class="menu-title">Post a Job</span>
                     <div className='mx-5 bg-lite'><svg xmlns="http://www.w3.org/2000/svg" width="100" height="20" fill="currentColor" class="bi bi-postcard" viewBox="0 0 16 16">
                       <path fill-rule="evenodd" d="M2 2a2 2 0 0 0-2 2v8a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V4a2 2 0 0 0-2-2zM1 4a1 1 0 0 1 1-1h12a1 1 0 0 1 1 1v8a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1zm7.5.5a.5.5 0 0 0-1 0v7a.5.5 0 0 0 1 0zM2 5.5a.5.5 0 0 1 .5-.5H6a.5.5 0 0 1 0 1H2.5a.5.5 0 0 1-.5-.5m0 2a.5.5 0 0 1 .5-.5H6a.5.5 0 0 1 0 1H2.5a.5.5 0 0 1-.5-.5m0 2a.5.5 0 0 1 .5-.5H6a.5.5 0 0 1 0 1H2.5a.5.5 0 0 1-.5-.5M10.5 5a.5.5 0 0 0-.5.5v3a.5.5 0 0 0 .5.5h3a.5.5 0 0 0 .5-.5v-3a.5.5 0 0 0-.5-.5zM13 8h-2V6h2z" />
@@ -80,6 +85,17 @@ function Sidebar() {
           </ul>
         </nav>
       </div>}
+      <Modal show={show} onHide={handleClose}>
+        <Modal.Body>
+
+          <div>
+            You have no credits
+          </div>
+          <button type='button' onClick={() => { handleClose() }}>x</button>
+
+        </Modal.Body>
+
+      </Modal>
     </>
   );
 }
