45ead44aa371e1bf1eafefd95506edc5dd1e4350
faraz9901 <mdfaraz@sparkgenieit.com>
2024-07-28 17:24:38 +0530

    Close and delete button is showing only the job is closed or expired

--- src/pages/company/jobs/JobList.js
+++ src/pages/company/jobs/JobList.js
@@ -159,20 +159,20 @@ function Joblist() {
                                                 <div class="col ">
                                                     <table class="table text-center " >
                                                         <thead>
-                                                            <tr className="" >
+                                                            <tr className="">
                                                                 <th className="text-start">Job Title</th>
                                                                 <th className="text-start">Job Reference</th>
                                                                 <th>Posted Date</th>
                                                                 <th className="text-center">Status</th>
                                                                 <th>Edit</th>
                                                                 <th>{assignJobs?.some((job) => job.status === "approved") && <span>Close</span>}</th>
-                                                                <th>Delete</th>
+                                                                <th>{assignJobs?.some((job) => job.status === "expired" || job.status === "closed") && <span>Delete</span>}</th>
                                                                 <th className="text-center">Applications</th>
                                                                 <th>{assignJobs?.some((job) => job.shortlisted > 0) && <span>Shortlisted</span>}</th>
                                                                 <th>{assignJobs?.some((job) => job.status === "expired" || job.status === "closed") && <span>Repost</span>}</th>
                                                             </tr>
-                                                            {assignJobs && assignJobs.length > 0 &&
-                                                                assignJobs.map((job, index) => {
+                                                            {
+                                                                assignJobs?.map((job, index) => {
                                                                     return <tr key={index}>
                                                                         <td className="text-start">{job.jobTitle}</td>
                                                                         <td className="text-start">{job.employjobreference}</td>
@@ -212,11 +212,13 @@ function Joblist() {
                                                                             }
                                                                         </td>
                                                                         <td>
-                                                                            <button type="button" class="btn  btn-xs btn-outline-danger" disabled={isLoading} onClick={() => setModal({ show: true, type: "delete", clickedJob: job })}>
-                                                                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash3" viewBox="0 0 16 16">
-                                                                                    <path d="M6.5 1h3a.5.5 0 0 1 .5.5v1H6v-1a.5.5 0 0 1 .5-.5M11 2.5v-1A1.5 1.5 0 0 0 9.5 0h-3A1.5 1.5 0 0 0 5 1.5v1H1.5a.5.5 0 0 0 0 1h.538l.853 10.66A2 2 0 0 0 4.885 16h6.23a2 2 0 0 0 1.994-1.84l.853-10.66h.538a.5.5 0 0 0 0-1zm1.958 1-.846 10.58a1 1 0 0 1-.997.92h-6.23a1 1 0 0 1-.997-.92L3.042 3.5zm-7.487 1a.5.5 0 0 1 .528.47l.5 8.5a.5.5 0 0 1-.998.06L5 5.03a.5.5 0 0 1 .47-.53Zm5.058 0a.5.5 0 0 1 .47.53l-.5 8.5a.5.5 0 1 1-.998-.06l.5-8.5a.5.5 0 0 1 .528-.47M8 4.5a.5.5 0 0 1 .5.5v8.5a.5.5 0 0 1-1 0V5a.5.5 0 0 1 .5-.5" />
-                                                                                </svg>
-                                                                            </button>
+                                                                            {job.status === "closed" || job.status === "expired" ?
+                                                                                <button type="button" class="btn  btn-xs btn-outline-danger" disabled={isLoading} onClick={() => setModal({ show: true, type: "delete", clickedJob: job })}>
+                                                                                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash3" viewBox="0 0 16 16">
+                                                                                        <path d="M6.5 1h3a.5.5 0 0 1 .5.5v1H6v-1a.5.5 0 0 1 .5-.5M11 2.5v-1A1.5 1.5 0 0 0 9.5 0h-3A1.5 1.5 0 0 0 5 1.5v1H1.5a.5.5 0 0 0 0 1h.538l.853 10.66A2 2 0 0 0 4.885 16h6.23a2 2 0 0 0 1.994-1.84l.853-10.66h.538a.5.5 0 0 0 0-1zm1.958 1-.846 10.58a1 1 0 0 1-.997.92h-6.23a1 1 0 0 1-.997-.92L3.042 3.5zm-7.487 1a.5.5 0 0 1 .528.47l.5 8.5a.5.5 0 0 1-.998.06L5 5.03a.5.5 0 0 1 .47-.53Zm5.058 0a.5.5 0 0 1 .47.53l-.5 8.5a.5.5 0 1 1-.998-.06l.5-8.5a.5.5 0 0 1 .528-.47M8 4.5a.5.5 0 0 1 .5.5v8.5a.5.5 0 0 1-1 0V5a.5.5 0 0 1 .5-.5" />
+                                                                                    </svg>
+                                                                                </button> : null
+                                                                            }
                                                                         </td>
                                                                         <td className="text-center">
                                                                             {job.status === "queue" || job.status === "review" ? <span>In Review</span> : null}
