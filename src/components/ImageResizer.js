410ccea1b66dce4ce25fa3e398151e089fd987a5
faraz9901 <mdfaraz@sparkgenieit.com>
2024-10-06 13:18:45 +0530

    Added the logo resize in companyprofile

--- src/components/ImageResizer.js
+++ src/components/ImageResizer.js
@@ -1,52 +1,163 @@
 import "./ImageResizer.css"
 
-import { useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 
-export default function Input({ width, height, setImage, defaultImg }) {
+export default function Input({ width, height, setImg, imgSrc }) {
+    const [croppedWidth, setCroppedWidth] = useState(0)
+    const [croppedHeight, setCroppedHeight] = useState(0)
     const imgRef = useRef(null);
+    const [originalImageUrl, setOriginalImageUrl] = useState(null)
 
-    const handleSliders = (e) => {
-        const value = e.target.value;
-        const name = e.target.name;
-        const valueToBeChanged = name === "width" ? width : height;
-        const percentage = value / 100;
-        const toBeAdded = valueToBeChanged * percentage;
-        imgRef.current.style[name] = valueToBeChanged + +toBeAdded + "px";
-    };
+    useEffect(() => {
+        fetch(imgSrc)
+            .then(response => response.blob())
+            .then(blob => {
+                const reader = new FileReader();
+                reader.readAsDataURL(blob);
+                reader.onload = function (e) {
+
+                    let image_url = e.target.result
+
+                    const img = new Image();
+
+                    img.src = image_url;
+
+                    img.onload = function () {
+                        const canvas = document.createElement("canvas");
+                        const ctx = canvas.getContext("2d");
+
+                        canvas.width = width
+                        canvas.height = height
+
+                        // Actual resizing
+                        ctx.drawImage(img, 0, 0, canvas.width, canvas.height);
+
+                        const dataurl = canvas.toDataURL("image/jpeg");
+                        setOriginalImageUrl(dataurl);
+                        imgRef.current.src = dataurl
+                        canvas.remove()
+                    }
+                }
+            })
+    }, [imgSrc])
+
+
+    const handleHeightSlider = (e) => {
+
+        const value = e.target.value
+
+        let newHeight = (height * (value / 100))
+
+        const img = new Image();
+
+        img.src = originalImageUrl;
+
+        img.onload = function () {
+
+            const canvas = document.createElement("canvas")
+
+            const ctx = canvas.getContext("2d")
+
+            canvas.width = width
+            canvas.height = height
+
+            ctx.fillStyle = 'white';
+            ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+            ctx.drawImage(img, croppedWidth, newHeight, width - (croppedWidth * 2), height - (newHeight * 2), 0, 0, width, height)
+
+            const imageUrl = canvas.toDataURL("image/jpeg")
+
+            imgRef.current.src = imageUrl
+
+            setCroppedHeight(newHeight)
+
+            canvas.toBlob((blob) => {
+                setImg(blob)
+            }, 'image/jpeg')
+
+            canvas.remove()
+        }
+
+    }
+
+    const handleWidthSliders = (e) => {
+
+        const value = e.target.value
+
+        let newWidth = (width * (value / 100))
+
+        const img = new Image();
+
+        img.src = originalImageUrl;
+
+        img.onload = function () {
+
+            const canvas = document.createElement("canvas")
+
+            const ctx = canvas.getContext("2d")
+
+            canvas.width = width
+            canvas.height = height
+
+
+            ctx.fillStyle = 'white';
+            ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+            ctx.drawImage(img, newWidth, croppedHeight, width - (newWidth * 2), height - (croppedHeight * 2), 0, 0, width, height)
+
+            const imageUrl = canvas.toDataURL("image/jpeg")
+
+            canvas.toBlob((blob) => {
+                setImg(blob)
+            }, 'image/jpeg')
+
+
+            imgRef.current.src = imageUrl
+
+            setCroppedWidth(newWidth)
+
+            canvas.remove()
+        }
+    }
+
 
     return (
         <div>
-            <div className="d-flex align-items-center">
+            <div className="d-flex gap-4 align-items-center">
                 <div
                     style={{
                         width: `${width}px`,
                         height: `${height}px`,
                         overflow: "hidden",
                     }}
-                    className="flex-grow-1 rounded"
+                    className="rounded border"
                 >
-                    <img className="rounded" ref={imgRef} src={defaultImg ? defaultImg : null} />
+                    <img className="rounded img-resize" ref={imgRef} />
                 </div>
-                <input
-                    type="range"
-                    name="height"
-                    onChange={handleSliders}
-                    defaultValue={0}
-                    min={-50}
-                    max={50}
-                    className="height-range"
-                />
+                <div style={{ width: "10px" }}>
+                    <input
+                        type="range"
+                        name="height"
+                        onChange={(e) => { handleHeightSlider(e) }}
+                        defaultValue={0}
+                        min={-25}
+                        max={25}
+                        className="height-slider"
+                        style={{ height: `${height}px` }}
+                    />
+                </div>
             </div>
             <input
                 type="range"
                 name="width"
-                onChange={handleSliders}
+                className="slider width-slider"
+                onChange={(e) => handleWidthSliders(e)}
                 defaultValue={0}
-                min={-50}
-                max={50}
+                min={-25}
+                max={25}
                 style={{ width: `${width}px` }}
             />
-
         </div>
     );
 }
--- src/pages/company/CompanyProfile.js
+++ src/pages/company/CompanyProfile.js
@@ -109,13 +109,13 @@ function CompanyProfile() {
       try {
         if (logo) {
           const fd = new FormData();
-          fd.append('file', logo);
+          fd.append('file', logo, "file.jpeg");
           const { data } = await companyService.uploadLogo(fd)
           obj1 = { ...obj1, logo: data.filename }
         }
         if (banner) {
           const fd = new FormData();
-          fd.append('file', banner);
+          fd.append('file', banner, "file.jpeg");
           const { data } = await httpUpload.post("/upload/banners?path=banners", fd)
           obj1 = { ...obj1, banner: data.filename }
         }
@@ -145,215 +145,208 @@ function CompanyProfile() {
 
   return (
     <>
-      <div class="container-fluid">
-        <div className="col-12 bg-white">
-          <div className="card-body container ">
+      <div class="container-fluid px-0 mt-4 bg-white">
+        <div className='container'>
 
-            <div className='mb-4'>
-              {companyBanner && companyBanner.length > 0 &&
-                <ImageResizer width={1000} height={250} setImage={setBanner} defaultImg={companyBanner} />
+
+          {companyBanner && companyBanner.length > 0 &&
+            <ImageResizer width={1000} height={250} setImg={setBanner} imgSrc={companyBanner} />
+          }
+
+          <div style={{ width: "1000px" }} className='d-flex mt-3 justify-content-between'>
+
+            <div className='d-flex gap-3 align-items-center'>
+
+              {companyLogo && companyLogo.length > 0 &&
+                <ImageResizer width={120} height={75} setImg={setLogo} imgSrc={companyLogo} />
               }
-            </div>
-            <div style={{ width: "1000px" }} className='d-flex justify-content-between'>
 
-              <div className='d-flex'>
+              <button type='button' style={{ backgroundColor: "blue" }} className='my-button align-self-start mt-1 text-white' onClick={() => { logoRef.current.click() }}>
 
-                {companyLogo && companyLogo.length > 0 &&
-                  <ImageResizer width={120} height={75} setImage={setLogo} defaultImg={companyLogo} />
-                }
+                Change Logo <br />
+                75 px height <br />
+                120px width
 
-                <button type='button' style={{ backgroundColor: "blue" }} className='my-button align-self-center text-white' onClick={() => { logoRef.current.click() }}>
-                  <small>
-                    Change Logo <br />
-                    75 px height <br />
-                    120px width
-                  </small>
-                </button>
+              </button>
 
-              </div>
-
-              <div>
-                <button type='button' style={{ backgroundColor: "#04045b" }} className='my-button px-2 text-white' onClick={() => { bannerRef.current.click() }} >
-                  <small>
-                    Change Banner
-                    <br />
-                    250 px height <br />
-                    1000px width
-
-                  </small>
-                </button>
-              </div>
             </div>
 
-            <div style={{ display: "none" }}>
-              <input ref={bannerRef} type="file" id="banner" onChange={onFileChange} />
-              <input ref={logoRef} type="file" id="logo" onChange={onFileChange} />
-            </div>
+            <div>
+              <button type='button' style={{ backgroundColor: "#04045b" }} className='my-button mt-1 text-white' onClick={() => { bannerRef.current.click() }} >
+                <small>
+                  Change Banner
+                  <br />
+                  250 px height <br />
+                  1000px width
 
-            <form onSubmit={submit} className="form-sample mt-3 p-4 ">
+                </small>
+              </button>
+            </div>
+          </div>
+
+          <div style={{ display: "none" }}>
+            <input ref={bannerRef} type="file" id="banner" accept='.jpg ,.png ,.jpeg' onChange={onFileChange} />
+            <input ref={logoRef} type="file" id="logo" accept='.jpg ,.png, .jpeg' onChange={onFileChange} />
+          </div>
+
+          <form onSubmit={submit} className="form-sample mt-3 p-4 ">
+            <div className="row">
               <div className="row">
-                <div className="row">
-                  <div className="form-group row">
-                    <label className="col-sm-3 col-form-label">Company Name<span className='text-danger'>*</span></label>
-                    <div className="col-sm-6">
-                      <input type="text" name='name' className="form-control" value={userData.name} onChange={handleInput} />
-                      {errors.name && <span className='text-danger'>{errors.name}</span>}
-                    </div>
+                <div className="form-group row">
+                  <label className="col-sm-3 col-form-label">Company Name<span className='text-danger'>*</span></label>
+                  <div className="col-sm-6">
+                    <input type="text" name='name' className="form-control" value={userData.name} onChange={handleInput} />
+                    {errors.name && <span className='text-danger'>{errors.name}</span>}
+                  </div>
+                </div>
+
+              </div>
+
+              <div className="row">
+
+                <div className="form-group row">
+                  <label className="col-sm-3 col-form-label">Address1<span className='text-danger'>*</span></label>
+                  <div className="col-sm-6">
+                    <input type="text" name='address1' className="form-control" value={userData.address1} onChange={handleInput} />
+                    {errors.address1 && <span className='text-danger'>{errors.address1}</span>}
+                  </div>
+                </div>
+              </div>
+              <div className="row">
+
+                <div className="form-group row">
+                  <label className="col-sm-3 col-form-label">Address2<span className='text-danger'>*</span></label>
+                  <div className="col-sm-6">
+                    <input type="text" className="form-control" name='address2' value={userData.address2} onChange={handleInput} />
+                    {errors.address2 && <span className='text-danger'>{errors.address2}</span>}
                   </div>
 
                 </div>
 
-                <div className="row">
-
-                  <div className="form-group row">
-                    <label className="col-sm-3 col-form-label">Address1<span className='text-danger'>*</span></label>
-                    <div className="col-sm-6">
-                      <input type="text" name='address1' className="form-control" value={userData.address1} onChange={handleInput} />
-                      {errors.address1 && <span className='text-danger'>{errors.address1}</span>}
-                    </div>
-                  </div>
-                </div>
-                <div className="row">
-
-                  <div className="form-group row">
-                    <label className="col-sm-3 col-form-label">Address2<span className='text-danger'>*</span></label>
-                    <div className="col-sm-6">
-                      <input type="text" className="form-control" name='address2' value={userData.address2} onChange={handleInput} />
-                      {errors.address2 && <span className='text-danger'>{errors.address2}</span>}
-                    </div>
-
-                  </div>
-
-                </div>
-                <div className="row">
-                  <div className="form-group row">
-                    <label className="col-sm-3 col-form-label">Address3<span className='text-danger'>*</span></label>
-                    <div className="col-sm-6">
-                      <input type="text" name='address3' className="form-control" value={userData.address3} onChange={handleInput} />
-                      {errors.address3 && <span className='text-danger'>{errors.address3}</span>}
-
-                    </div>
-                  </div>
-
-
-                </div>
-
-                <div className="row">
-
-                  <div className="form-group row">
-                    <label className="col-sm-3 col-form-label">City<span className='text-danger'>*&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; </span></label>
-                    <div className="col-sm-6">
-                      <input type="text" name='city' className="form-control" value={userData.city} onChange={handleInput} />
-                      {errors.city && <span className='text-danger'>{errors.city}</span>}
-                    </div>
+              </div>
+              <div className="row">
+                <div className="form-group row">
+                  <label className="col-sm-3 col-form-label">Address3<span className='text-danger'>*</span></label>
+                  <div className="col-sm-6">
+                    <input type="text" name='address3' className="form-control" value={userData.address3} onChange={handleInput} />
+                    {errors.address3 && <span className='text-danger'>{errors.address3}</span>}
 
                   </div>
                 </div>
-                <div className="row">
-                  <div className="form-group row">
-                    <label className="col-sm-3 col-form-label">PostCode<span className='text-danger'>*</span></label>
-                    <div className="col-sm-6">
-                      <input type="text" className="form-control" name='postalCode' value={userData.postalCode} onChange={handleInput} />
-                      {errors.postalCode && <span className='text-danger'>{errors.postalCode}</span>}
 
-                    </div>
-                  </div>
-                </div>
-                <div className="row">
-                  <div className="form-group row">
-                    <label className="col-sm-3 col-form-label">Phone<span className='text-danger'>*&nbsp; &nbsp; &nbsp;</span></label>
-                    <div className="col-sm-6">
-                      <input type="text" name='phone' className="form-control" value={userData.phone} onChange={handleInput} />
-                      {errors.phone && <span className='text-danger'>{errors.phone}</span>}
-                    </div>
-                  </div>
-                </div>
-                <div className="row">
-                  <div className="form-group row">
-                    <label className="col-sm-3 col-form-label">Email<span className='text-danger'>*&nbsp; &nbsp; &nbsp;</span></label>
-                    <div className="col-sm-6">
-                      <input type="text" className="form-control" name='email' disabled value={userData.email} onChange={handleInput} />
 
-                    </div>
+              </div>
+
+              <div className="row">
+
+                <div className="form-group row">
+                  <label className="col-sm-3 col-form-label">City<span className='text-danger'>*&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; </span></label>
+                  <div className="col-sm-6">
+                    <input type="text" name='city' className="form-control" value={userData.city} onChange={handleInput} />
+                    {errors.city && <span className='text-danger'>{errors.city}</span>}
+                  </div>
+
+                </div>
+              </div>
+              <div className="row">
+                <div className="form-group row">
+                  <label className="col-sm-3 col-form-label">PostCode<span className='text-danger'>*</span></label>
+                  <div className="col-sm-6">
+                    <input type="text" className="form-control" name='postalCode' value={userData.postalCode} onChange={handleInput} />
+                    {errors.postalCode && <span className='text-danger'>{errors.postalCode}</span>}
+
                   </div>
                 </div>
-                <div className="row">
-                  <div className="form-group row">
-                    <label className="col-sm-3 col-form-label">Contact Person<span className='text-danger'>*&nbsp; &nbsp;</span></label>
-                    <div className="col-sm-6">
-                      <input type="text" className="form-control" name='contact' value={userData.contact} onChange={handleInput} />
-                      {errors.contact && <span className='text-danger'>{errors.contact}</span>}
-                    </div>
+              </div>
+              <div className="row">
+                <div className="form-group row">
+                  <label className="col-sm-3 col-form-label">Phone<span className='text-danger'>*&nbsp; &nbsp; &nbsp;</span></label>
+                  <div className="col-sm-6">
+                    <input type="text" name='phone' className="form-control" value={userData.phone} onChange={handleInput} />
+                    {errors.phone && <span className='text-danger'>{errors.phone}</span>}
                   </div>
                 </div>
-                <div className="row">
-                  <div className="form-group row">
-                    <label className="col-sm-3 col-form-label">WebSite<span className='text-danger'>*&nbsp; &nbsp; </span></label>
-                    <div className="col-sm-6">
-                      <input type="text" name='website' className="form-control" value={userData.website} onChange={handleInput} />
-                      {errors.website && <span className='text-danger'>{errors.website}</span>}
-                    </div>
+              </div>
+              <div className="row">
+                <div className="form-group row">
+                  <label className="col-sm-3 col-form-label">Email<span className='text-danger'>*&nbsp; &nbsp; &nbsp;</span></label>
+                  <div className="col-sm-6">
+                    <input type="text" className="form-control" name='email' disabled value={userData.email} onChange={handleInput} />
+
                   </div>
                 </div>
-                <div className="row ">
-                  <div className="form-group row d-flex align-items-center">
-                    <label className="col-sm-3 col-form-label ">Youtube URL</label>
-                    <div className="col-sm-6 d-flex gap-2  align-items-center " >
-                      <div className='w-100'>
-                        <input type="text" className="form-control" name='youtubeUrl' value={userData.youtubeUrl} onChange={handleInput} />
-                      </div>
-                      {userData.youtubeUrl && <div className='position-relative'  >
-                        <iframe
-                          ref={youtubeRef}
-                          className='rounded no-scrollbar'
-                          width="150px"
-                          height="80px"
-                          src={`https://www.youtube.com/embed/${getYoutubeVideoId(userData.youtubeUrl)}`}
-                          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-                          allowFullScreen
-                        />
-                        {/* <span role='button' onClick={() => { youtubeRef.current.requestFullscreen() }} style={{ right: "40px", top: "5px" }} className='position-absolute'>
+              </div>
+              <div className="row">
+                <div className="form-group row">
+                  <label className="col-sm-3 col-form-label">Contact Person<span className='text-danger'>*&nbsp; &nbsp;</span></label>
+                  <div className="col-sm-6">
+                    <input type="text" className="form-control" name='contact' value={userData.contact} onChange={handleInput} />
+                    {errors.contact && <span className='text-danger'>{errors.contact}</span>}
+                  </div>
+                </div>
+              </div>
+              <div className="row">
+                <div className="form-group row">
+                  <label className="col-sm-3 col-form-label">WebSite<span className='text-danger'>*&nbsp; &nbsp; </span></label>
+                  <div className="col-sm-6">
+                    <input type="text" name='website' className="form-control" value={userData.website} onChange={handleInput} />
+                    {errors.website && <span className='text-danger'>{errors.website}</span>}
+                  </div>
+                </div>
+              </div>
+              <div className="row ">
+                <div className="form-group row d-flex align-items-center">
+                  <label className="col-sm-3 col-form-label ">Youtube URL</label>
+                  <div className="col-sm-6 d-flex gap-2  align-items-center " >
+                    <div className='w-100'>
+                      <input type="text" className="form-control" name='youtubeUrl' value={userData.youtubeUrl} onChange={handleInput} />
+                    </div>
+                    {userData.youtubeUrl && <div className='position-relative'  >
+                      <iframe
+                        ref={youtubeRef}
+                        className='rounded no-scrollbar'
+                        width="150px"
+                        height="80px"
+                        src={`https://www.youtube.com/embed/${getYoutubeVideoId(userData.youtubeUrl)}`}
+                        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                        allowFullScreen
+                      />
+                      {/* <span role='button' onClick={() => { youtubeRef.current.requestFullscreen() }} style={{ right: "40px", top: "5px" }} className='position-absolute'>
                               <FaYoutube fontSize={70} fill="#FF3D00" />
                             </span> */}
-                      </div>}
-
-                    </div>
-                  </div>
-                </div>
-
-
-                <div className="row">
-                  <div className="form-group row">
-                    <label className="col-form-label">Info (Describe your company business in less than 250 words)</label>
-
-                    <div>
-                      <MdxEditor value={info} setValue={setInfo} />
-                    </div>
-
+                    </div>}
 
                   </div>
                 </div>
-
-
-
-
               </div>
+
 
               <div className="row">
-                <div className="col-md-12">
-                  <button type="submit" className="btn btn-gradient-primary float-end">Save</button>
+                <div className="form-group row">
+                  <label className="col-form-label">Info (Describe your company business in less than 250 words)</label>
+
+                  <div>
+                    <MdxEditor value={info} setValue={setInfo} />
+                  </div>
+
+
                 </div>
               </div>
 
-            </form>
-          </div>
+
+
+
+            </div>
+
+            <div className="row">
+              <div className="col-md-12">
+                <button type="submit" className="btn btn-gradient-primary float-end">Save</button>
+              </div>
+            </div>
+
+          </form>
         </div>
-        {/* </div> */}
-
-
-
-      </div>
-
+      </div >
 
       <Hourglass
         visible={loader}
