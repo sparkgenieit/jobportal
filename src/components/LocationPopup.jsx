dc138b26391ab39e0fa6d7ca9e60d48b35f6956c
vintron-kiran <vintron@richlabz.com>
2025-05-25 15:00:50 +0530

    Fix Region map

--- src/components/LocationPopup.jsx
+++ src/components/LocationPopup.jsx
@@ -1,17 +1,42 @@
+import { useEffect, useState } from 'react';
 import { RxCross1 } from 'react-icons/rx';
 import './location-popup.css';
 import { Modal } from "react-bootstrap";
 import { useDispatch, useSelector } from 'react-redux';
 import { setLocation } from '../helpers/slices/generalSlice';
 import ToastViewer from "../components/common/ToastViewer";
+import http from "../helpers/http";
 
 export default function LocationPopup() {
     const dispatch = useDispatch();
     const locationPopup = useSelector((state) => state.general.location);
-    const info = useSelector((state) => state.general.info);
-console.log('info',info);
+
+    const [info, setInfo] = useState([]);
+    const [loading, setLoading] = useState(false);
+
+    useEffect(() => {
+        const fetchCityInfo = async () => {
+            if (!locationPopup.city) return;
+
+            setLoading(true);
+            try {
+                const citySlug = locationPopup.city.toLowerCase();
+                const response = await http.get(`/cms/?category=regions&page=${citySlug}`);
+                setInfo(response.data.content || []);
+            } catch (error) {
+                console.error("Failed to fetch city info:", error);
+                setInfo([]);
+            } finally {
+                setLoading(false);
+            }
+        };
+
+        fetchCityInfo();
+    }, [locationPopup.city]);
+
     const handleClose = () => {
         dispatch(setLocation({ show: false }));
+        setInfo([]); // Optional: reset local info on close
     };
 
     return (
@@ -26,18 +51,21 @@ console.log('info',info);
                     </div>
 
                     <div style={{ overflowX: "auto" }} className='flex gap-2 justify-between my-3'>
-                        <img style={{ height: "20vh", width: "100%" }} className="rounded" src="https://content.r9cdn.net/rimg/dimg/09/c2/d0aa16e0-city-2575-166c0a657e0.jpg?width=1366&height=768&xhint=1673&yhint=1229&crop=true" />
-                        <img style={{ height: "20vh", width: "100%" }} className="rounded" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQptUYv5XAlbkhMn5MjKP2ZaYdJ-XadHrkEENVzISWgyX1cNMs1lmCbsCpny0uo" />
-                        <img style={{ height: "20vh", width: "100%" }} className="rounded" src="https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcRqDexPvTsSD7a1UZ3TxHRw9bE5nD_ZlZZlZAWAQbfGaE-GEszVIkIvsSE-tx6o" />
-                        <img style={{ height: "20vh", width: "100%" }} className="rounded" src="https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcRqDexPvTsSD7a1UZ3TxHRw9bE5nD_ZlZZlZAWAQbfGaE-GEszVIkIvsSE-tx6o" />
-                        <img style={{ height: "20vh", width: "100%" }} className="rounded" src="https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcRqDexPvTsSD7a1UZ3TxHRw9bE5nD_ZlZZlZAWAQbfGaE-GEszVIkIvsSE-tx6o" />
+                        {[...Array(5)].map((_, i) => (
+                            <img key={i}
+                                style={{ height: "20vh", width: "100%" }}
+                                className="rounded"
+                                src="https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcRqDexPvTsSD7a1UZ3TxHRw9bE5nD_ZlZZlZAWAQbfGaE-GEszVIkIvsSE-tx6o"
+                                alt={`Location ${i + 1}`}
+                            />
+                        ))}
                     </div>
 
                     <div>
-                        {info?.length > 0 ? (
-                            
-                            <ToastViewer key={`${locationPopup.city}`} content={info} loading={!info}/>
-                           
+                        {loading ? (
+                            <p>Loading content...</p>
+                        ) : info?.length > 0 ? (
+                            <ToastViewer key={locationPopup.city} content={info} loading={false} />
                         ) : (
                             <p>No activities found for this region.</p>
                         )}
